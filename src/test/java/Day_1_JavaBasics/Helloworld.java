fc62a76bc7e08f456c343406fc25f407a2886d69
rajAutomtaion <raj116629@gmail.com>
2025-06-19 00:57:50 +0530

    helloWorld

---
+++ src/test/java/Day_1_JavaBasics/Helloworld.java
@@ -0,0 +1,11 @@
+package Day_1_JavaBasics;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello,world");
+
+	}
+
+}
