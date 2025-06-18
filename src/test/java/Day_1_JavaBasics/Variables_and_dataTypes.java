6229c275485c5fd867ad1596650eb896d4f72d78
rajAutomtaion <raj116629@gmail.com>
2025-06-19 01:03:53 +0530

    variable

---
+++ src/test/java/Day_1_JavaBasics/Variables_and_dataTypes.java
@@ -0,0 +1,24 @@
+package Day_1_JavaBasics;
+
+public class Variables_and_dataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//variable is nothing but a placeholder to store the data
+		//data types: int , String etc
+		
+		int a=20;
+		String name ="Raj";
+		char c='a';
+		float f= 23;
+		
+		System.out.println(a);
+		System.out.println(name);
+		System.out.println(c);
+		System.out.println(f);
+		
+
+	}
+
+}
