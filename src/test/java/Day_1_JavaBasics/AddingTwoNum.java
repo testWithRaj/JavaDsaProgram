16acd6158d617f31de1fda158b0764cb4d749d63
rajAutomtaion <raj116629@gmail.com>
2025-06-19 01:14:10 +0530

    variable practise

---
+++ src/test/java/Day_1_JavaBasics/AddingTwoNum.java
@@ -0,0 +1,15 @@
+package Day_1_JavaBasics;
+
+public class AddingTwoNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 20;
+		int b=20;
+		int sum =a+b;
+		
+		System.out.println("Sum is: "+sum);
+
+	}
+
+}
---
+++ src/test/java/Day_1_JavaBasics/variable_practise.java
@@ -0,0 +1,19 @@
+package Day_1_JavaBasics;
+
+public class variable_practise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name ="Raj";
+		int age=29;
+		float salary =30000;
+		char gender='M';
+		
+		System.out.println("Name: "+name);
+		System.out.println("Age: "+age);
+		System.out.println("Salary: "+salary);
+		System.out.println("Gender: "+gender);
+
+	}
+
+}
