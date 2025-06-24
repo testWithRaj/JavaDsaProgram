d62189f82f4aba91cec5f26f2215701ea1e6611d
rajAutomtaion <raj116629@gmail.com>
2025-06-24 22:49:08 +0530

    array

---
+++ src/test/java/Day_2_inputFromKeyboard/Day_3.java
@@ -0,0 +1,12 @@
+package Day_2_inputFromKeyboard;
+
+public class Day_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
---
+++ src/test/java/Day_5_Array/BasicsOfArray.java
@@ -0,0 +1,16 @@
+package Day_5_Array;
+
+public class BasicsOfArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//What is an Array
+		//Array is used to store multiple values in a single variable with same data type
+
+		
+		 int numbers [] = {1,2,3,4,5};
+		 
+		System.out.println(numbers[4]);
+
+}
+}
