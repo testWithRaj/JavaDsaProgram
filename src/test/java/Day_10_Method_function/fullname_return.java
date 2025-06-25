73562edb6d9a14616188cdd6fb955efce1bc67f3
rajAutomtaion <raj116629@gmail.com>
2025-06-26 01:08:12 +0530

    return

--- src/test/java/Day_10_Method_function/fullname_return.java
+++ src/test/java/Day_10_Method_function/fullname_return.java
@@ -2,10 +2,18 @@ package Day_10_Method_function;
 
 public class fullname_return {
 	
-	public String fullname(String)
+	public String fullname(String a, String b)
+	{
+		String fname=a+ " " +b;
+		return fname;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		fullname_return fn= new fullname_return();
+	String result=	fn.fullname("Aarti", "Raj");
+	System.out.println(result);
 
 	}
 
