0ca1d24ae1b92379344062a5c5ad5921a01afc52
rajAutomtaion <raj116629@gmail.com>
2025-06-26 00:45:23 +0530

    multiplication

--- src/test/java/Day_10_Method_function/multiplication.java
+++ src/test/java/Day_10_Method_function/multiplication.java
@@ -1,10 +1,19 @@
 package Day_10_Method_function;
 
 public class multiplication {
+	
+	public void multi(int a,int b)
+	{
+	int m=a*b;	
+	
+	System.out.println(m);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		multiplication ml = new multiplication();
+		ml.multi(10, 8);
 	}
 
 }
