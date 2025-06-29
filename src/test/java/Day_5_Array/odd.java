4386e7fad93bee4bfdf32a7d23d0e25d2ddf6274
rajAutomtaion <raj116629@gmail.com>
2025-06-29 23:33:54 +0530

    odd number

--- src/test/java/Day_5_Array/odd.java
+++ src/test/java/Day_5_Array/odd.java
@@ -5,7 +5,7 @@ public class odd {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int arr[]= {1,2,3,4,5,6,6,8,9,11};
+		int arr[]= {1,2,3,4,5,6,6,8,9,11,14};
 		int count=0;
 		
 		for(int i=0;i<arr.length;i++)
