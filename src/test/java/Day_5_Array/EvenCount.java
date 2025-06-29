3460333f8de09039dcad1848cac6d7d22e3600ce
rajAutomtaion <raj116629@gmail.com>
2025-06-29 23:35:08 +0530

    evenCount

--- src/test/java/Day_5_Array/EvenCount.java
+++ src/test/java/Day_5_Array/EvenCount.java
@@ -4,7 +4,7 @@ public class EvenCount {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int arr[]= {2,3,4,6,8,9,11,12,12};
+		int arr[]= {2,3,4,6,8,9,11,12,12,132};
 		int count=0;
 		for(int i=0;i<arr.length;i++)
 		{
